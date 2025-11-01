cc50c0ae6afb52730c996e5e9c2cb0b99aa798aa
js15-gh <159508084+js15-gh@users.noreply.github.com>
2025-11-01 15:44:22 -0700

    Update blue_mission.py

--- blue_mission.py
+++ blue_mission.py
@@ -7,11 +7,21 @@ def main_program():
     gizmo.change_speed(200)
     gizmo.go_forward(9)
     gizmo.turn_left_degrees(47)
-    gizmo.go_forward(9)
-    gizmo.d_turn_angle(200, 220)
-    gizmo.go_reverse(5)
-
-
+    gizmo.go_forward(8.7)
+    gizmo.d_turn_angle(300, 220)
+    gizmo.go_reverse(4.5)
+    gizmo.d_turn_angle(-300, 220)
+    gizmo.go_reverse(2.9)
+    gizmo.d_turn_angle(300, 200)
+    gizmo.turn_left_degrees(50)
+    gizmo.d_turn_angle(-300, 200)
+    gizmo.go_forward(15.5)
+    gizmo.turn_right_degrees(80)
+    gizmo.go_forward(2.8)
+    gizmo.d_turn_angle_back2zero(400, 200)
+    gizmo.go_reverse(3)
+    gizmo.turn_left_degrees(83)
+    gizmo.go_forward(12)
 
 
 main_program()

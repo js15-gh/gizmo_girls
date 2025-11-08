2ced6c57907ab6096b4706e5e13d6e9ca32a2ad1
StarGazer-qwerty <shahanvi@hotmail.com>
2025-11-08 10:16:23 -0800

    hiii

--- pink_mission.py
+++ pink_mission.py
@@ -7,13 +7,13 @@ def main_program():
     gizmo.change_speed(300)
     gizmo.go_reverse(24.0)
     gizmo.go_forward(6)
-    gizmo.turn_right_degrees(50)
-    gizmo.go_reverse(11)
-    gizmo.turn_left_degrees(98)
+    gizmo.turn_right_degrees(55)
+    gizmo.go_reverse(10.5)
+    gizmo.turn_left_degrees(120)
     # Map Reveal
     gizmo.go_reverse(9)
     gizmo.c_turn_angle(500,300)
-    # gizmo.go_forward(7.0)
+    gizmo.go_forward(7.0)
 
 
 

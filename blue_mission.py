02ad9b5fbacadae90a159e954e04ee4fc967f828
js15-gh <159508084+js15-gh@users.noreply.github.com>
2025-11-08 15:15:50 -0800

    YAy it work

--- blue_mission.py
+++ blue_mission.py
@@ -13,18 +13,23 @@ def main_program():
     gizmo.d_turn_angle(-300, 220)
     gizmo.go_reverse(2.9)
     gizmo.d_turn_angle(300, 200)
-    gizmo.turn_left_degrees(55)
+    gizmo.change_speed(50)
+    gizmo.turn_left_degrees(47) #55
+    gizmo.change_speed(200)
     gizmo.d_turn_angle(-300, 200)
-    gizmo.go_forward(16.67)
+    gizmo.go_forward(16.6) # o 16.7
     gizmo.turn_right_degrees(110)
     gizmo.wait(67)
     gizmo.go_forward(1)
     gizmo.turn_left_degrees(12)
     gizmo.go_forward(3.9)
-    gizmo.d_turn_angle_back2zero(400,90)
+    gizmo.d_turn_angle_back2zero(400,100)
     gizmo.go_reverse(5)
     gizmo.turn_left_degrees(83)
-    gizmo.go_forward(12)
+    # gizmo.change_speed(400)
+    # gizmo.go_forward(40)
+    # gizmo.turn_left_degrees(52)
+    # gizmo.go_forward(6.7)
 
 
 main_program()

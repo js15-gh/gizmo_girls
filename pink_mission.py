f543098b28bf1a563280651b5aefbddb7bd22ea9
StarGazer-qwerty <shahanvi@hotmail.com>
2025-11-08 16:06:25 -0800

    changed missiob
    fixed mission

--- pink_mission.py
+++ pink_mission.py
@@ -4,16 +4,19 @@ import gizmo
 def main_program():
     #Surface Brushing 
     #start 5th line form start/left
-    gizmo.change_speed(300)
-    gizmo.go_reverse(24.0)
+    gizmo.change_speed(150)
+    gizmo.go_reverse(24.67)
     gizmo.go_forward(6)
     gizmo.turn_right_degrees(55)
-    gizmo.go_reverse(10.5)
-    gizmo.turn_left_degrees(120)
+    gizmo.go_reverse(9.4)
+    gizmo.turn_left_degrees(138)
     # Map Reveal
-    gizmo.go_reverse(9)
+    gizmo.change_speed(100)
+    gizmo.go_reverse(10)
     gizmo.c_turn_angle(500,300)
     gizmo.go_forward(7.0)
+    gizmo.turn_right_degrees(50)
+
 
 
 

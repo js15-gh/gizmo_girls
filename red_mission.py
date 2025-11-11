d0959384d0df7534dddbf0634e091fbb98ff075b
aahana67 <aahanaseth27@gmail.com>
2025-11-11 14:52:50 -0800

    i fixed miss
    very good now

---
+++ red_mission.py
@@ -0,0 +1,20 @@
+import gizmo
+
+def main_program():
+    print("Starting main program sequence...")
+    print("-" * 20)
+
+
+#2) Bring items to forum
+    gizmo.go_forward(7)
+    gizmo.turn_right_degrees(47)
+    gizmo.go_forward(10)
+
+
+
+
+
+
+
+# --- RUN THE MAIN PROGRAM ---
+main_program()

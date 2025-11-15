560256732998a44776b7b896bb4878aaacf25e7a
aahana67 <aahanaseth27@gmail.com>
2025-11-15 15:56:53 -0800

    Update red_mission.py

--- red_mission.py
+++ red_mission.py
@@ -6,19 +6,18 @@ def main_program():
 
 
 #2) Bring items to forum
-    gizmo.go_forward(15.5)
-    gizmo.change_turn_speed(20)
-    gizmo.turn_right_degrees(85)
-    gizmo.go_forward(10)
-    gizmo.go_reverse(9)
-    gizmo.change_turn_speed(40)
-    gizmo.turn_right_degrees(23)
-    gizmo.change_speed(200)
-    gizmo.go_forward(30)
-    gizmo.turn_right_degrees(26)
-    gizmo.go_forward(9)
-    gizmo.turn_left_degrees(5)
-
+    #gizmo.go_forward(16)
+    #gizmo.turn_right_degrees(85)
+    #gizmo.go_forward(10)
+    #gizmo.go_reverse(9)
+    #gizmo.change_turn_speed(40)
+    #gizmo.turn_right_degrees(23)
+    #gizmo.change_speed(200)
+    #gizmo.go_forward(31)
+    #gizmo.turn_right_degrees(22.5)
+    #gizmo.go_forward(8.5)
+    gizmo.d_turn_angle(1000,1200)
+    #gizmo.go_reverse(9)
 
 
 

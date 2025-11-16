2f5459438edfb3fbdc19002c8e32c0a4ccd720f7
StarGazer-qwerty <shahanvi@hotmail.com>
2025-11-15 18:16:51 -0800

    Update pink_mission.py

--- pink_mission.py
+++ pink_mission.py
@@ -6,16 +6,21 @@ def main_program():
     #start 5th line form start/left
     gizmo.change_speed(150)
     gizmo.go_reverse(24.67)
-    gizmo.go_forward(6)
-    gizmo.turn_right_degrees(55)
-    gizmo.go_reverse(9.4)
-    gizmo.turn_left_degrees(138)
-    # Map Reveal
-    gizmo.change_speed(100)
-    gizmo.go_reverse(10)
-    gizmo.c_turn_angle(500,300)
-    gizmo.go_forward(7.0)
-    gizmo.turn_right_degrees(50)
+    gizmo.go_forward(7)
+    gizmo.d_turn_angle(-400, 250)
+    gizmo.go_reverse(4)
+    gizmo.d_turn_angle(200, 350)
+    gizmo.wait(1.67)
+    gizmo.d_turn_angle(-400, 275)
+    # gizmo.turn_right_degrees(55)
+    # gizmo.go_reverse(9.4)
+    # gizmo.turn_left_degrees(120)
+    # # Map Reveal
+    # gizmo.change_speed(100)
+    # gizmo.go_reverse(10)
+    # gizmo.c_turn_angle(500,600)
+    # gizmo.go_forward(7.0)
+    # gizmo.turn_right_degrees(50)
 
 
 

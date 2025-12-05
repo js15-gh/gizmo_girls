70f319c892a233fb5c61c7939bbf19a6e9bbc5e9
StarGazer-qwerty <shahanvi@hotmail.com>
2025-12-04 18:09:44 -0800

    Update yellow_mission.py
    cdaef

--- yellow_mission.py
+++ yellow_mission.py
@@ -5,13 +5,13 @@ def main_program():
     #start last second line from end/right
     gizmo.change_speed(500)
     gizmo.go_forward(38.5) #39
-    gizmo.turn_single_motor_B(200,-325) #340
+    gizmo.turn_single_motor_B(200,-345) #340 -> 325
     gizmo.c_turn_angle(200,-500)
     gizmo.d_turn_time(500,0.7)
     gizmo.wait_for_reflection(14)
     gizmo.wait(200)
     gizmo.d_turn_time(-300,0.9)
-    gizmo.c_turn_angle(200,52) #40
+    gizmo.c_turn_angle(200,80) #40
     gizmo.wait(100)
     #gizmo.c_turn_angle(200,-20)
     gizmo.go_reverse(5)
@@ -19,16 +19,18 @@ def main_program():
     gizmo.c_turn_angle(500,100)
     gizmo.turn_right_degrees(25)
     gizmo.go_forward(3)
-    gizmo.turn_right_degrees(20)
+    gizmo.turn_right_degrees(31) #20
     gizmo.d_turn_time(500,0.7)
     gizmo.go_forward(13.5) #12.5
-    gizmo.d_turn_time(-500,0.7)
-    gizmo.go_reverse(5)
-    gizmo.turn_right_degrees(130)
-    gizmo.go_forward(9)
-    gizmo.turn_left_degrees(90)
-    gizmo.go_forward(20)
-    #coolll
+    gizmo.turn_left_degrees(23)
+    gizmo.d_turn_time(-100,1.1)
+    gizmo.turn_right_degrees(23)
+    gizmo.go_reverse(2)
+    gizmo.turn_right_degrees(140)
+    gizmo.go_forward(14)
+    gizmo.turn_left_degrees(80)
+    gizmo.go_forward(25)
+    # #coolll
 
 
 

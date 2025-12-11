930c6581172ee882dea1de8b3ce69e09224b4a9b
js15-gh <159508084+js15-gh@users.noreply.github.com>
2025-12-10 17:36:16 -0800

    yippeeeee
    yayyyyy

---
+++ brown_mission.py
@@ -0,0 +1,21 @@
+import gizmo
+
+def main_program():
+    print("Starting main program sequence...")
+    print("-" * 20)
+
+    gizmo.change_speed(200)
+    gizmo.go_forward(9)
+    gizmo.turn_left_degrees(91)
+    gizmo.go_forward(21)
+    gizmo.turn_left_degrees(86)
+    gizmo.go_forward(5.05)
+    gizmo.turn_left_degrees(14)
+    gizmo.d_turn_angle(2000, 650)
+    gizmo.go_reverse(10)
+    gizmo.change_speed(500)
+    gizmo.go_forward(5)
+    gizmo.turn_right_degrees(110)
+    gizmo.go_forward(10)
+    gizmo.turn_left_degrees(30)
+    gizmo.go_forward(30)

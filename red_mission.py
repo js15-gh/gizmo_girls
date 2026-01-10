7698e147969d609f4b52f28f07921df005cc0887
js15-gh <159508084+js15-gh@users.noreply.github.com>
2026-01-10 13:00:02 -0800

    hehhee

--- red_mission.py
+++ red_mission.py
@@ -16,8 +16,8 @@ def main_program():
     gizmo.d_turn_angle(40,99)#
     gizmo.wait(0.5)#1
     gizmo.change_speed(300)#200
-    gizmo.go_forward(2.5)#removed the "false" so instead drive forwards first before lifting
-    gizmo.d_turn_angle(300,160) #180
+    gizmo.go_forward(1.7)#removed the "false" so instead drive forwards first before lifting
+    gizmo.d_turn_time(100,2) #180
     #gizmo.go_forward(3)#2.5
     gizmo.turn_right_degrees(10)
     gizmo.change_speed(300)#500
@@ -29,6 +29,32 @@ def main_program():
     gizmo.turn_right_degrees(41)
     gizmo.go_forward(7.67)
     gizmo.go_reverse(7)
+    # gizmo.change_speed(200)#300
+    # gizmo.go_forward(6.2)
+    # gizmo.turn_right_degrees(37)
+    # gizmo.go_forward(11.5) #12.25
+    # gizmo.d_turn_angle(-300,300)
+    # gizmo.change_speed(200)
+    # gizmo.turn_left_degrees(10.5)#10
+    # gizmo.turn_right_degrees(25)#30
+    # gizmo.d_turn_angle(40,99)#
+    # gizmo.wait(0.5)#1
+    # gizmo.change_speed(300)
+    # gizmo.d_turn_angle(40,99)#
+    # gizmo.wait(0.5)#1
+    # gizmo.change_speed(300)#200
+    # #gizmo.d_turn_angle(300,180,False) #100
+    # gizmo.d_turn_angle(300,160,False) #180
+    # gizmo.go_forward(3)#2.5
+    # gizmo.turn_right_degrees(10)
+    # gizmo.change_speed(300)#500
+    # gizmo.go_reverse(12)
+    # gizmo.wait(30)
+    # gizmo.go_forward(19)
+    # #gizmo.turn_right_degrees(42)
+    # gizmo.turn_right_degrees(41)
+    # gizmo.go_forward(7.67)
+    # gizmo.go_reverse(7)
 #2) Bring items to forum
     # gizmo.change_speed(250)
     # gizmo.go_forward(19)

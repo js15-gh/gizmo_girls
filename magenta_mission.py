935d899ec2f4fdbe62ae97380181b10d3ea66c72
js15-gh <159508084+js15-gh@users.noreply.github.com>
2026-01-24 14:38:22 -0800

    yess

---
+++ magenta_mission.py
@@ -0,0 +1,11 @@
+import gizmo
+
+
+def main_program():
+    gizmo.reset_defaults()
+    gizmo.go_forward(30)
+    gizmo.turn_left_degrees(90)
+    gizmo.go_forward(21.27)
+    gizmo.turn_left_degrees(45)
+    gizmo.go_forward(6.6)
+    gizmo.c_turn_angle(500,290)

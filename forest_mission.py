e430b0dc3e3caa0d47b0a069f8e3e788a50a5aa7
StarGazer-qwerty <shahanvi@hotmail.com>
2026-03-21 13:30:40 -0700

    Update forest_mission.py
    yggy

--- forest_mission.py
+++ forest_mission.py
@@ -1,8 +1,23 @@
-from pybricks.hubs import PrimeHub
-from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor, ForceSensor
-from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
-from pybricks.robotics import DriveBase
-from pybricks.tools import wait, StopWatch
+import gizmo
 
-hub = PrimeHub()
+def main_program():
+    gizmo.reset_defaults
+
+    gizmo.change_speed(200)
+    gizmo.go_forward(29)
+    gizmo.change_turn_speed(50)
+    gizmo.turn_left_degrees(90)
+    gizmo.go_forward(14.5)
+    gizmo.d_turn_angle(200,67)
+    gizmo.turn_right_degrees(90)
+    gizmo.go_forward(6.5)
+    gizmo.d_turn_angle(200,-70)
+    gizmo.go_reverse(5.5)
+    gizmo.turn_left_degrees(90)
+    gizmo.go_forward(15)
+    gizmo.turn_left_degrees(45)
+    gizmo.go_forward(10)
+    gizmo.turn_left_degrees(45) 
+    gizmo.go_forward(3)
+    gizmo.d_turn_angle(200,200)
 
